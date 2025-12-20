6d685a431a28dc527c01e478f68a3aa6e01eda7a
VitorCamposDev <v_campos_dev@outlook.com>
2025-12-19 23:34:52 -0300

    fix

--- src/auth/dto/complete-registration.dto.ts
+++ src/auth/dto/complete-registration.dto.ts
@@ -1,7 +1,7 @@
 import { Type } from 'class-transformer';
 import { ValidateNested, IsOptional } from 'class-validator';
-import { RegisterClinicDto } from '../clinics/dto/register-clinic.dto';
-import { RegisterPsychologistDto } from '../psychologist-profile/dto/register-psychologist.dto';
+import { RegisterClinicDto } from '../../clinics/dto/register-clinic.dto';
+import { RegisterPsychologistDto } from '../../psychologist-profile/dto/register-psychologist.dto';
 
 export class CompleteOwnerRegistrationDto {
     @ValidateNested()
--- src/staff-profile/entity/staff-profile.entity.ts
+++ src/staff-profile/entity/staff-profile.entity.ts
@@ -1,4 +1,4 @@
-import { User } from '../users/entities/user.entity';
+import { User } from '../../users/entities/user.entity';
 import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm';
 
 @Entity('staff_profiles')
