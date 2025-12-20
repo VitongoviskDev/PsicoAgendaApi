60f3d522af76a5d980073200d0b2e42ce7090f9a
VitorCamposDev <v_campos_dev@outlook.com>
2025-12-19 22:20:53 -0300

    buidl

--- server.ts
+++ server.ts
@@ -9,12 +9,9 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: [
-            'https://psicoagenda.app',
-            'https://www.psicoagenda.app',
-        ],
+        origin: 'https://psicoagenda.app',
         credentials: true,
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
         allowedHeaders: 'Content-Type, Authorization',
     });
 
