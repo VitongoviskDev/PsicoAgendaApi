81b60529c043ebe36d0c33761147296cae1846d1
VitorCamposDev <v_campos_dev@outlook.com>
2025-12-24 17:25:18 -0300

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,12 +2,22 @@ import 'dotenv/config';
 import { DataSource } from 'typeorm';
 import type { DataSourceOptions } from 'typeorm';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export const dataSourceOptions: DataSourceOptions = {
     type: 'postgres',
     url: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
-    entities: ['src/**/*.entity.ts'],
-    migrations: ['src/migrations/*.ts'],
+    entities: [
+        isProd ?
+            'dist/**/*.entity.js' :
+            'src/**/*.entity.ts'
+    ],
+    migrations: [
+        isProd ?
+            'dist/migrations/*.js' :
+            'src/migrations/*.ts'
+    ]
 };
 
 export const AppDataSource = new DataSource(dataSourceOptions);
